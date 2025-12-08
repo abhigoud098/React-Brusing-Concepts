40a195a9b0d90bb9a829cc0290101c01d93d661e
Abhishek <abhigoud198484@gmail.com>
2025-12-08 06:11:28 +0530

    Portfolio project

---
+++ Basic-Portfolio/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Hero from "./components/hero/Hero.jsx";
+import About from "./components/about/About.jsx";
+import Skills from "./components/skills/Skills.jsx";
+
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Skills/>
+    </>
+  );
+}
+
+export default App;
