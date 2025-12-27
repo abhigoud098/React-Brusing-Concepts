9e703c5544f2a0d58cc389d607337accc99c6b32
Abhishek <abhigoud198484@gmail.com>
2025-12-27 08:48:55 +0530

    New things add in mini project

--- Mini-Project/src/App.jsx
+++ Mini-Project/src/App.jsx
@@ -4,8 +4,34 @@ import Layout from "./Layout";
 import Home from "../src/components/home/Home";
 import Products from "../src/components/products/Products";
 import Cart from "../src/components/cart/Cart";
+import { useState } from "react";
 
 function App() {
+  const [cartItems, setCartItems] = useState([]);
+
+  const productData = (product) => {
+    setCartItems((prevCart) => {
+      // Check if the product already exists in cart
+      const existingItem = prevCart.find((item) => item.id === product.id);
+
+      if (existingItem) {
+        // Update quantity or price if it already exists
+        return prevCart.map(
+          (item) =>
+            item.id === product.id
+              ? {
+                  ...item,
+                  quantity: item.quantity + 1,
+                } // Clicked product edit and add it
+              : item // UnClicked product as it is add it
+        );
+      } else {
+        // Add new item to cart
+        return [...prevCart, { ...product, quantity: 1,}];
+      }
+    });
+  };
+
   const router = createBrowserRouter([
     {
       path: "/",
@@ -17,11 +43,11 @@ function App() {
         },
         {
           path: "/products",
-          element: <Products />,
+          element: <Products productData={productData} />,
         },
         {
           path: "/cart",
-          element: <Cart />,
+          element: <Cart cartItems={cartItems} />,
         },
       ],
     },
